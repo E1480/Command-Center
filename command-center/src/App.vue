cfa6ec5bc2902bcc7f2a788205398df9d761cc7f
E1480 <113873052+E1480@users.noreply.github.com>
2024-05-14 10:37:02 +0000

    Commit

--- command-center/src/App.vue
+++ command-center/src/App.vue
@@ -2,9 +2,6 @@
 import { ref } from 'vue';
 import NavComp from './components/Nav.vue';
 
-
-const title = ref("Command Center")
-
 </script>
 
 <template>
@@ -12,10 +9,29 @@ const title = ref("Command Center")
         <NavComp :title="title" />
     </nav>
     <main>
-        
+
     </main>
 </template>
 
 <style scoped>
-
+    .container{
+        display: inline-block;
+        place-items: center;
+        padding: 1em;
+        margin: 3em;
+        width: 15em;
+        height: 5em;
+        border: rgb(128, 128, 128) 1px solid;
+        border-radius: 10px;
+        color: white;
+        transition: 0.5s;
+    }
+    .container:hover{
+        box-shadow: -5px 5px rgba(51, 51, 51, 0.603);
+        transition: 0.5s;
+    }
+    .container button{
+        width: 100%;
+        color: red;
+    }
 </style>
