6acca7907f7f5a7a5dbae38f34da6295fd7016e4
E1480 <113873052+E1480@users.noreply.github.com>
2024-05-14 11:00:43 +0000

    Commit

--- command-center/src/App.vue
+++ command-center/src/App.vue
@@ -1,37 +1,22 @@
 <script setup>
 import { ref } from 'vue';
 import NavComp from './components/Nav.vue';
+import Tile from './components/Tile.vue'
+
+const CenterTitle = ref('Command Center')
+const TileName = ref('Title')
 
 </script>
 
 <template>
     <nav>
-        <NavComp :title="title" />
+        <NavComp :title="CenterTitle" />
     </nav>
     <main>
-
+        <Tile :title="TileName"/>
     </main>
 </template>
 
 <style scoped>
-    .container{
-        display: inline-block;
-        place-items: center;
-        padding: 1em;
-        margin: 3em;
-        width: 15em;
-        height: 5em;
-        border: rgb(128, 128, 128) 1px solid;
-        border-radius: 10px;
-        color: white;
-        transition: 0.5s;
-    }
-    .container:hover{
-        box-shadow: -5px 5px rgba(51, 51, 51, 0.603);
-        transition: 0.5s;
-    }
-    .container button{
-        width: 100%;
-        color: red;
-    }
+
 </style>
---
+++ command-center/src/components/Tile.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+    title: String,
+    text_align: String
+})
+
+
+</script>
+
+<template>
+
+<div class="container" :class="text_align || 'center'">
+    <h2>{{ title }}</h2>
+</div>
+
+
+</template>
+
+<style scoped>
+
+    .container{
+        display: inline-block;
+        padding: 1em;
+        margin: 3em;
+        /* Dimensions */
+        width: 15em;
+        height: 5em;
+        /* Border */
+        border: rgba(128, 128, 128, 0.123) 1px solid;
+        border-radius: 10px;
+        color: white;
+        transition: 0.5s;
+    }
+    .container:hover{
+        box-shadow: -5px 5px rgba(51, 51, 51, 0.603);
+        transition: 0.5s;
+    }
+
+    .center{
+        text-align: center;
+    }
+    .left{
+        text-align: left;
+    }
+    .right{
+        text-align: right;
+    }
+</style>
